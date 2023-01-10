92bab3e458a661aeaee1ba6949f4411ebcf20df5
smjx93 <smjx@proton.me>
2023-01-10 11:55:08 +0100

    try to create a perfect 16 x 16 grid

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-// Sets important constants and variables
+ // Get the container div
+ let container = document.getElementById("grid-container");
 
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
-
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+ // Create a loop to create the grid squares
+ for (let i = 0; i < 256; i++) {
+   // Create a new div
+   let square = document.createElement("div");
+   // Add the "square" class to the div
+   square.classList.add("square");
+   // Append the square to the container
+   container.appendChild(square);
+ };
